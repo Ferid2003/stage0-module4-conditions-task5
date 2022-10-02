880473df63b34b7a2cc396be3412b9a3fa93cbca
Ferid <feridagazade157@gmail.com>
2022-10-02 22:12:42 +0400

    implemented switch statements

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year%100!=0 && year%4==0 ||year==2000) {
+            System.out.println("leap");
+        }else{
+            System.out.println("not leap");
+        }
     }
 }
